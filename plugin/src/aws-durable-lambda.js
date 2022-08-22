eead9b7743b4f6716356c2aede38ffcc027c8a68
Sean Dawson <contact@seandawson.info>
2022-08-22 17:43:25 +1000

    build: filter out non js files when copying staging files (fixes #42)
    - This fixes the warnings about ambiguous handlers as only the javascript files are copied to the staging area before bundling
    - This might make it harder to get nice stack traces when an error occurs in lambda invocations, but the map files will still be under node_modules in a pinch. It was either this, or get the end user to add special config for serverless-webpack. We might need to revisit this in the future if it becomes an issue.
    - Using the more mature 'serverless-plugin-warmup' library as a reference, rejigged the hooks to be more specific

--- plugin/src/aws-durable-lambda.js
+++ plugin/src/aws-durable-lambda.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { join } = require('path');
+const { join, extname, dirname } = require('path');
 const dynamodb = require('./resources/dynamodb');
 const sqs = require('./resources/sqs');
 const { copy, remove } = require('fs-extra');
@@ -12,6 +12,25 @@ const functionStagingDirectoryName = '.adl';
 const getFunctionStagingDirectory = (serverless) => {
   const basePath = serverless.config.servicePath;
   return join(basePath, functionStagingDirectoryName);
+};
+
+/**
+ * The filter used when copying over the boilerplate files to the .adl/ staging dir.
+ *
+ * Will ensure that only Javascript files are copied and not extraneous files (e.g. .map files)
+ * that confuse serverless-webpack and cause it to show warnings.
+ *
+ * @param {*} src the source path of the file that is being copied
+ * @returns true if the file should be copied, otherwise false
+ */
+const filterOutAnythingButJs = (src) => {
+  const extension = extname(src);
+  // Checking for empty string is the easiest way to determine if the src
+  // is a directory (which we always want to allow so the copy will recurse)
+  // We have control of the source directory and know we won't be putting any
+  // files without extensions in there so it seems like a safe enough way to do
+  // it without get fs.stat involved.
+  return extension === '' || extension.toLocaleLowerCase() === '.js';
 };
 
 function addResources(serverless) {
@@ -79,23 +98,21 @@ async function copyBoilerplateFunctionCode(serverless) {
   serverless.cli.log(
     `[aws-durable-lambda] Copying boilerplate function code from [${source}] to [${destination}]...`
   );
-  await copy(source, destination);
+  await copy(source, destination, {
+    filter: filterOutAnythingButJs,
+  });
 }
 
 class AWSDurableLambda {
   constructor(serverless) {
     this.hooks = {
-      //this.serverless.config.servicePath
-      'package:cleanup': async function () {
+      'after:package:initialize': async function () {
+        addResources(serverless);
         await cleanupBoilerplateFunctionCode(serverless);
         return copyBoilerplateFunctionCode(serverless);
       },
-      'package:finalize': async function () {
+      'after:package:createDeploymentArtifacts': async function () {
         await cleanupBoilerplateFunctionCode(serverless);
-      },
-
-      initialize: function () {
-        addResources(serverless);
       },
     };
   }
